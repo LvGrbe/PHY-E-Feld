a13ffe91d825e9dcc223ab0c9f55a41fa15b49d3
Simon Hardt <s-hardt@live.de>
2017-02-06 11:34:51 +0100

    Das Zentrum des Kreises ist jetzt in der Mitte.

--- E-Feld-Simulation/E-Feld-Simulation/Simulation.cpp
+++ E-Feld-Simulation/E-Feld-Simulation/Simulation.cpp
@@ -21,7 +21,9 @@ void Simulation::renderer_Teilchen()
 	//Zeichen die Vektor Punkte 
 	for (int i = 0; i < Teilchen_vec.size(); i++)
 	{
-		sim_RenderWindow->draw(Teilchen_vec[i].Erstelle_Kreis_Ladung(5.0));	
+		auto d = Teilchen_vec[i].Erstelle_Kreis_Ladung(5.0);
+		d.move(sf::Vector2f(-5, -5));
+		sim_RenderWindow->draw(d);	
 
 	}
 
