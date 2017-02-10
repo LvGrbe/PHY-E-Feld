0cb3580572f1f84adb73093d1133fa513b91c7db
Leopold v Grabe <l.v.grabe@posteo.de>
2017-02-10 17:19:29 +0100

    kleine änderung

--- E-Feld-Simulation/E-Feld-Simulation/Darstellung_Fähnchen.h
+++ E-Feld-Simulation/E-Feld-Simulation/Darstellung_Fähnchen.h
@@ -13,7 +13,7 @@ public:
 	Darstellung_Fähnchen(std::shared_ptr<sf::RenderWindow> Window,std::vector<Punktladung>* teilchen);
 	~Darstellung_Fähnchen();
 	std::vector<Punktladung>* Teilchen_vec;
-	std::shared_ptr<sf::RenderWindow> dRf_Window;
+	std::shared_ptr<sf::RenderWindow> df_Window;
 	/*std::vector<Pfeil>* ladungen;*/
 	float Länge_Vektor(sf::Vector2f a);
 	sf::Vector2f Elektrische_Feldstärke_Vektoren_Viele(sf::Vector2f punkt);
