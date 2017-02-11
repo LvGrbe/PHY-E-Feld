dc0d49f06bf4048ea67b6db4fb4b74e090ea870a
Leopold v Grabe <l.v.grabe@posteo.de>
2017-02-11 19:34:55 +0100

    g

--- E-Feld-Simulation/E-Feld-Simulation/Darstellung_Fähnchen.h
+++ E-Feld-Simulation/E-Feld-Simulation/Darstellung_Fähnchen.h
@@ -14,7 +14,6 @@ public:
 	~Darstellung_Fähnchen();
 	std::vector<Punktladung>* Teilchen_vec;
 	std::shared_ptr<sf::RenderWindow> df_Window;
-	sf::Vector2f Elektrische_Feldstärke_Vektoren_Viele(sf::Vector2f punkt);
 	void Draw();
 	sf::VertexArray Erstelle_Arrow(sf::Vector2i mouspos);
 };
